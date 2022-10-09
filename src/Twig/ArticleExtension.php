d0b4216fed84a3bc45143da63b5f4ec74fcafb8c
Fred-QS <66546741+fred-qs@users.noreply.github.com>
2022-10-09 09:24:08 +0200

    Split article content

---
+++ src/Twig/ArticleExtension.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class ArticleExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            new TwigFilter('split_content', [$this, 'splitContent', 'is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            //new TwigFunction('function_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function splitContent($value): array
+    {
+        $chunk = explode('. ', trim($value));
+        $middle = (int) ceil((count($chunk)-1)/2);
+        $firstPart = [];
+        $lastPart = [];
+        $cnt = 0;
+        foreach ($chunk as $row) {
+            if ($cnt <= $middle) {
+                $firstPart[] = trim($row);
+            } else {
+                $lastPart[] = trim($row);
+            }
+            $cnt++;
+        }
+
+        return [
+            implode('. ', $firstPart) . '.',
+            implode('. ', $lastPart)
+        ];
+    }
+}
